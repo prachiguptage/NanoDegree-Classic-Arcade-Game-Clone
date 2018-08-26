8eb6ef9140c80e28905b2f6e5a60260c907a0f61
Prachi Gupta <prachiguptage@gmail.com>
2018-08-25 19:52:18 -0700

    adding player to board

--- js/app.js
+++ js/app.js
@@ -14,6 +14,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+   
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,14 +25,26 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class players{
+	constructor(){
+		this.x = 0;
+		this.y = 0;
+		this.sprite = 'images/char-boy.png';
+	}
 
-
+	//draw player on current x and y co ordinate
+	render(){
+		ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+	}
+	
+	}
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-var allEnemies=[new Enemy()];
-// Place the player object in a variable called player
 
+// Place the player object in a variable called player
+var player = new players();
 
 
 
